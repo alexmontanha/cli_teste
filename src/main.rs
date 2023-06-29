178568c7885bce5e34d622b6a2c393399760d9ef
Alexandre de Oliveira <alexmontanha@hotmail.com>
2023-06-29 20:49:33 -0300

    Handling arguments

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,18 @@ use std::env;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    println!("{:?}", args);
+
+    if args.len() < 2 {
+        println!("Usage: {} <input> [output]", args[0]);
+        return;
+    } else if args.len() > 3 {
+        println!("Too many arguments");
+        return;
+    } else {
+        if args.len() == 3 {
+            println!("{:?}_{:?}.out", args[1], args[2]);
+        } else {
+            println!("{:?}.in", args[1]);
+        }
+    }
 }
